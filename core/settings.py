3a4ee9b94b4870b92694f59f1e1fe9092e03ad8d
Khayot Razzakov <114242863+Khayotbek1@users.noreply.github.com>
2025-10-24 16:55:15 +0500

    user models

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #apps
     'users.apps.UsersConfig',
+    'teachers.apps.TeachersConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
+
--- users/models.py
+++ users/models.py
@@ -19,3 +19,42 @@ class User(AbstractUser):
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='STUDENT')
 
+    phone_regex = RegexValidator(
+        regex=r'^\+998\d{9}$',
+        message='Telephone number must be entered in the format: +998XXXXXXXXX'
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=11, unique=True)
+    additional_phone = models.CharField(validators=[phone_regex], max_length=11, blank=True, null=True)
+
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    address = models.TextField(blank=True, null=True)
+    region = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    telegram_id = models.CharField(max_length=100, blank=True, null=True, unique=True)
+    telegram_username = models.CharField(max_length=100, blank=True, null=True)
+
+    sms_notifications = models.BooleanField(default=True)
+    telegram_notifications = models.BooleanField(default=False)
+    email_notifications = models.BooleanField(default=False)
+
+    is_active_user = models.BooleanField(default=True)
+    last_login_ip = models.GenericIPAddressField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Meta:
+    ordering = ['-created_at']
+    verbose_name = 'Foydalanuvchi'
+    verbose_name_plural = 'Foydalanuvchilar'
+    indexes = [
+        models.Index(fields=['role', 'is_active_user']),
+        models.Index(fields=['phone_number']),
+    ]
+    def __str__(self):
+        return f"{self.get_full_name()} ({self.get_role_display()})"
